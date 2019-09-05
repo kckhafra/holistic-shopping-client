0f07dd3c674d86eb4faa884a39ee502ec6292012
Kuntcheramen <kckhafra@gmail.com>
2019-09-05 15:23:57 -0400

    update server for heroku on config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://enigmatic-beach-64239.herokuapp.com/',
     TOKEN_KEY: 'holistic-client-auth-token',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
