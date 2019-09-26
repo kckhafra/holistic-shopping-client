49d700ae36e95379b066c25efc986f6215742997
Kuntcheramen <kckhafra@gmail.com>
2019-09-26 08:40:04 -0400

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    // API_ENDPOINT: 'https://enigmatic-beach-64239.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://enigmatic-beach-64239.herokuapp.com/api',
     TOKEN_KEY: 'holistic-client-auth-token',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
